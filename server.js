284e1e9149305d18f6a0256bdf97025d07bcfdcc
Henry K <mykim1891@hotmail.com>
2018-10-24 15:49:34 -0700

    Update to format

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
-const mongoose = require("mongoose");
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
 
-const noteRouter = require("./Notes/noteController");
-const userRouter = require("./Users/userController");
+const noteRouter = require('./Notes/noteController');
+const userRouter = require('./Users/userController');
 
 const server = express();
 const corsOption = {
-  origin: "http://localhost:3000",
-  credentials: true
+	origin: 'http://localhost:3000',
+	credentials: true
 };
 
 //middleware
@@ -30,26 +30,25 @@ server.use(cors(corsOption));
 
 // // mlab deployment
 mongoose
-  .connect("mongodb://admin:adminpw@ds239940.mlab.com:39940/lambda-note_h")
-
-  .then(mongo => {
-    console.log("-=- connected to mongo database -=-");
-  })
-  .catch(err => {
-    console.log("!Error, trouble connecting to mongo DB!");
-  });
+	.connect('mongodb://admin:adminpw@ds239940.mlab.com:39940/lambda-note_h')
+	.then((mongo) => {
+		console.log('-=- connected to mongo database 1 -=-');
+	})
+	.catch((err) => {
+		console.log('!Error, trouble connecting to mongo DB!');
+	});
 
 // sanity check
-server.get("/", (req, res) => {
-  res.json({ Message: "API is working!" });
+server.get('/', (req, res) => {
+	res.json({ Message: 'API is working!' });
 });
 
 // routes
-server.use("/api/notes", noteRouter);
-server.use("/api/users", userRouter);
+server.use('/api/notes', noteRouter);
+server.use('/api/users', userRouter);
 
 const port = process.env.PORT || 5000;
-server.listen(port, err => {
-  if (err) console.log(err);
-  console.log(`=== Server running on port ${port} ===`);
+server.listen(port, (err) => {
+	if (err) console.log(err);
+	console.log(`=== Server running on port ${port} ===`);
 });
